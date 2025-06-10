416af8978c5be51b69c87a76916c2907a4087955
ZeinTonconi <zein@DESKTOP-37TT2H8.localdomain>
2025-06-10 00:37:21 -0400

    feat[670]: Implementar el endpoint POST /register-user

---
+++ AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/AuthMicroserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package upb.edu.AuthMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMicroserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMicroserviceBackendApplication.class, args);
+	}
+
+}
