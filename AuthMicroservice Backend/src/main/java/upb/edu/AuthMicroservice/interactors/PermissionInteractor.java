5629c97d0c822f894a46d868358b697b6932082f
Nigthshitf <burgos3du@gmail.com>
2025-08-19 08:32:02 -0400

    fix/lastCommitDevelop

--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/interactors/PermissionInteractor.java
+++ AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/interactors/PermissionInteractor.java
@@ -1,29 +1,4 @@
-package upb.edu.AuthMicroservice.interactors;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import upb.edu.AuthMicroservice.models.Permission;
-import upb.edu.AuthMicroservice.services.PermissionService;
-
-
-@Component
-public class PermissionInteractor {
-
-
-    @Autowired
-    private PermissionService permissionService;
-
-
-    public Permission createPermission(Permission permission) {
-        return permissionService.createPermission(permission);
-    }
-
-    public boolean deletePermission(String id) {
-        return permissionService.deletePermission(id);
-    }
-
-}
 
 package upb.edu.AuthMicroservice.interactors;
 
@@ -50,5 +25,9 @@ public class PermissionInteractor {
 
     public List<Permission> getAllPermissions() {
         return permissionService.getAllPermissions();
+    }
+    
+    public boolean deletePermission(String id) {
+        return permissionService.deletePermission(id);
     }
 }
