d6d5af47f46a293bc9b688d29aabc2d3bf3fb0e6
mel79847 <soncovillegasm@gmail.com>
2025-08-29 22:29:38 -0400

    feat(routes): redirect / to /forgot-password

--- frontend/login-microfrontend/src/App.tsx
+++ frontend/login-microfrontend/src/App.tsx
@@ -1,17 +1,29 @@
-﻿import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+﻿import { useState } from "react";
+import Login from "./components/Login";
+import ModeSelector from "./components/ModeSelector";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
 
 function App() {
+  const [mode, setMode] = useState("center");
+
   return (
     <div className="background-full">
       <div className="app-container">
+        {/* <ModeSelector mode={mode} setMode={setMode} /> */}
+        {/* <Login mode={mode} /> */}
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Navigate to="/forgot-password" replace />} />
-            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route
+              path="/forgot-password"
+              element={<ForgotPasswordPage />}
+            />
+            <Route
+              path="/reset-password"
+              element={<ResetPasswordPage />}
+            />
           </Routes>
         </BrowserRouter>
       </div>
